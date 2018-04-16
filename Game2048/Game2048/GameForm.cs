dd4eda73c87b54fd63555a256eaa1932abb04e88
scgyong <scgyong@gmail.com>
2018-04-16 20:20:22 +0900

    2차원 Array 에 label 16 개 넣어놓음.

--- Game2048/Game2048/GameForm.cs
+++ Game2048/Game2048/GameForm.cs
@@ -10,10 +10,19 @@ using System.Windows.Forms;
 
 namespace Game2048 {
     public partial class GameForm : Form {
+        Label[,] board = new Label[4, 4];
         public GameForm()
         {
             InitializeComponent();
+            board = new Label[4, 4] {
+                { label13, label14, label15, label16 },
+                { label9, label10, label11, label12 },
+                { label5, label6, label7, label8 },
+                { label1, label2, label3, label4 },
+            };
         }
+
+        int x = 0, y = 0;
 
         private int score = 0;
         private void increaseScore(int diff)
@@ -24,21 +33,37 @@ namespace Game2048 {
 
         private void leftButton_Click(object sender, EventArgs e)
         {
+            if (x > 0) {
+                x--;
+                board[y, x].Text = score.ToString();
+            }
             increaseScore(-1);
         }
 
         private void rightButton_Click(object sender, EventArgs e)
         {
+            if (x < 3) {
+                x++;
+                board[y, x].Text = score.ToString();
+            }
             increaseScore(1);
         }
 
         private void upButton_Click(object sender, EventArgs e)
         {
+            if (y > 0) {
+                y--;
+                board[y, x].Text = score.ToString();
+            }
             increaseScore(10);
         }
 
         private void downButton_Click(object sender, EventArgs e)
         {
+            if (y < 3) {
+                y++;
+                board[y, x].Text = score.ToString();
+            }
             increaseScore(-10);
 
         }
