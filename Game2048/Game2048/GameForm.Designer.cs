3b37a05d48186c2babcf3f56a97335a459ed345c
scgyong <scgyong@gmail.com>
2018-04-19 11:46:50 +0900

    숫자 사각형 크기 키우고 배경색 지정.

--- Game2048/Game2048/GameForm.Designer.cs
+++ Game2048/Game2048/GameForm.Designer.cs
@@ -50,7 +50,7 @@
             // 
             // leftButton
             // 
-            this.leftButton.Location = new System.Drawing.Point(12, 368);
+            this.leftButton.Location = new System.Drawing.Point(13, 555);
             this.leftButton.Name = "leftButton";
             this.leftButton.Size = new System.Drawing.Size(75, 23);
             this.leftButton.TabIndex = 0;
@@ -60,7 +60,7 @@
             // 
             // upButton
             // 
-            this.upButton.Location = new System.Drawing.Point(93, 354);
+            this.upButton.Location = new System.Drawing.Point(94, 541);
             this.upButton.Name = "upButton";
             this.upButton.Size = new System.Drawing.Size(75, 23);
             this.upButton.TabIndex = 0;
@@ -70,7 +70,7 @@
             // 
             // downButton
             // 
-            this.downButton.Location = new System.Drawing.Point(93, 383);
+            this.downButton.Location = new System.Drawing.Point(94, 570);
             this.downButton.Name = "downButton";
             this.downButton.Size = new System.Drawing.Size(75, 23);
             this.downButton.TabIndex = 0;
@@ -80,7 +80,7 @@
             // 
             // rightButton
             // 
-            this.rightButton.Location = new System.Drawing.Point(174, 368);
+            this.rightButton.Location = new System.Drawing.Point(175, 555);
             this.rightButton.Name = "rightButton";
             this.rightButton.Size = new System.Drawing.Size(75, 23);
             this.rightButton.TabIndex = 0;
@@ -90,147 +90,179 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 286);
+            this.label1.BackColor = System.Drawing.Color.GreenYellow;
+            this.label1.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 380);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(100, 100);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(91, 286);
+            this.label2.BackColor = System.Drawing.Color.GreenYellow;
+            this.label2.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 380);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.Size = new System.Drawing.Size(100, 100);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label1";
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(172, 286);
+            this.label3.BackColor = System.Drawing.Color.GreenYellow;
+            this.label3.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(230, 380);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.Size = new System.Drawing.Size(100, 100);
             this.label3.TabIndex = 1;
-            this.label3.Text = "label1";
+            this.label3.Text = "0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(259, 286);
+            this.label4.BackColor = System.Drawing.Color.GreenYellow;
+            this.label4.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(340, 380);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.Size = new System.Drawing.Size(100, 100);
             this.label4.TabIndex = 1;
-            this.label4.Text = "label1";
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 245);
+            this.label5.BackColor = System.Drawing.Color.GreenYellow;
+            this.label5.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 270);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.Size = new System.Drawing.Size(100, 100);
             this.label5.TabIndex = 1;
-            this.label5.Text = "label1";
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(91, 245);
+            this.label6.BackColor = System.Drawing.Color.GreenYellow;
+            this.label6.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(120, 270);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 12);
+            this.label6.Size = new System.Drawing.Size(100, 100);
             this.label6.TabIndex = 1;
-            this.label6.Text = "label1";
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(172, 245);
+            this.label7.BackColor = System.Drawing.Color.GreenYellow;
+            this.label7.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(230, 270);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 12);
+            this.label7.Size = new System.Drawing.Size(100, 100);
             this.label7.TabIndex = 1;
-            this.label7.Text = "label1";
+            this.label7.Text = "0";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(259, 245);
+            this.label8.BackColor = System.Drawing.Color.GreenYellow;
+            this.label8.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(340, 270);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(38, 12);
+            this.label8.Size = new System.Drawing.Size(100, 100);
             this.label8.TabIndex = 1;
-            this.label8.Text = "label1";
+            this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label9
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(13, 199);
+            this.label9.BackColor = System.Drawing.Color.GreenYellow;
+            this.label9.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(10, 160);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(38, 12);
+            this.label9.Size = new System.Drawing.Size(100, 100);
             this.label9.TabIndex = 1;
-            this.label9.Text = "label1";
+            this.label9.Text = "0";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label10
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(91, 199);
+            this.label10.BackColor = System.Drawing.Color.GreenYellow;
+            this.label10.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(120, 160);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(38, 12);
+            this.label10.Size = new System.Drawing.Size(100, 100);
             this.label10.TabIndex = 1;
-            this.label10.Text = "label1";
+            this.label10.Text = "0";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label11
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(172, 199);
+            this.label11.BackColor = System.Drawing.Color.GreenYellow;
+            this.label11.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(230, 160);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(38, 12);
+            this.label11.Size = new System.Drawing.Size(100, 100);
             this.label11.TabIndex = 1;
-            this.label11.Text = "label1";
+            this.label11.Text = "0";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label12
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(259, 199);
+            this.label12.BackColor = System.Drawing.Color.GreenYellow;
+            this.label12.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(340, 160);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(38, 12);
+            this.label12.Size = new System.Drawing.Size(100, 100);
             this.label12.TabIndex = 1;
-            this.label12.Text = "label1";
+            this.label12.Text = "0";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label13
             // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(13, 149);
+            this.label13.BackColor = System.Drawing.Color.GreenYellow;
+            this.label13.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(10, 50);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(38, 12);
+            this.label13.Size = new System.Drawing.Size(100, 100);
             this.label13.TabIndex = 1;
-            this.label13.Text = "label1";
+            this.label13.Text = "0";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label14
             // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(91, 149);
+            this.label14.BackColor = System.Drawing.Color.GreenYellow;
+            this.label14.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(120, 50);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(38, 12);
+            this.label14.Size = new System.Drawing.Size(100, 100);
             this.label14.TabIndex = 1;
-            this.label14.Text = "label1";
+            this.label14.Text = "0";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label15
             // 
-            this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(172, 149);
+            this.label15.BackColor = System.Drawing.Color.GreenYellow;
+            this.label15.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(230, 50);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(38, 12);
+            this.label15.Size = new System.Drawing.Size(100, 100);
             this.label15.TabIndex = 1;
-            this.label15.Text = "label1";
+            this.label15.Text = "0";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label16
             // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(259, 149);
+            this.label16.BackColor = System.Drawing.Color.GreenYellow;
+            this.label16.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(340, 50);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(38, 12);
+            this.label16.Size = new System.Drawing.Size(100, 100);
             this.label16.TabIndex = 1;
-            this.label16.Text = "label1";
+            this.label16.Text = "0";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // scoreLabel
             // 
@@ -246,7 +278,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 412);
+            this.ClientSize = new System.Drawing.Size(588, 626);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label12);
