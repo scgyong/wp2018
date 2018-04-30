a0cba513fb1fa509b338aa556ed99cbc43f65700
scgyong <scgyong@gmail.com>
2018-04-30 20:17:29 +0900

    Move up/down/left/right 에 대해 Translate delegate 를 사용하여 하나의 함수로 만든다.

--- Game2048/Game2048/GameForm.cs
+++ Game2048/Game2048/GameForm.cs
@@ -82,6 +82,94 @@ namespace Game2048 {
             string str = value != 0 ? value.ToString() : "";
             board[y, x].Text = str;
         }
+        private delegate void Translator(int x, int y, out int ox, out int oy);
+        private bool move(Translator translator)
+        {
+            bool moved = false;
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 4; x++) {
+                    int ox, oy; translator(x, y, out ox, out oy);
+                    int number = get(ox, oy);
+                    if (number == 0) {
+                        // 비어있으므로 채워온다.
+                        for (int x2 = x + 1; x2 < 4; x2++) {
+                            int ox2, oy2; translator(x2, y, out ox2, out oy2);
+                            number = get(ox2, oy2);
+                            if (number > 0) {
+                                // 숫자를 찾으면 해당 숫자를 x, y 로 가져오고 찾은 곳은 0 으로 만든다.
+                                set(ox, oy, number);
+                                set(ox2, oy2, 0);
+                                moved = true;
+                                break;
+                            }
+                        }
+                        if (number == 0) {
+                            // 끝으로 갔는데도 숫자가 없으면 이 줄은 더 이상 하지 않아도 된다.
+                            break;
+                        }
+                    }
+                    for (int x2 = x + 1; x2 < 4; x2++) {
+                        int ox2, oy2; translator(x2, y, out ox2, out oy2);
+                        int n2 = get(ox2, oy2);
+                        if (number == n2) {
+                            // 같은 숫자가 발견되면 2배 값을 적고 발견된 곳은 0 으로 만든다.
+                            int value = 2 * number;
+                            set(ox, oy, value);
+                            set(ox2, oy2, 0);
+                            increaseScore(value);
+                            moved = true;
+                            break;
+                        } else if (n2 > 0) {
+                            // 다른 숫자가 발견되었으므로 더 이상 할 일이 없다.
+                            break;
+                        }
+                    }
+                }
+            }
+            return moved;
+        }
+
+        private void translateLeft(int x, int y, out int ox, out int oy)
+        {
+            ox = x; oy = y;
+        }
+        private void translateRight(int x, int y, out int ox, out int oy)
+        {
+            ox = 3 - x; oy = y;
+        }
+        private void translateUp(int x, int y, out int ox, out int oy)
+        {
+            ox = y; oy = x;
+        }
+        private void translateDown(int x, int y, out int ox, out int oy)
+        {
+            ox = 3 - y; oy = 3 - x;
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool moved = false;
+            switch (e.KeyCode) {
+            case Keys.Left:
+                moved = move(translateLeft);
+                break;
+            case Keys.Right:
+                moved = move(translateRight);
+                break;
+            case Keys.Up:
+                moved = move(translateUp);
+                break;
+            case Keys.Down:
+                moved = move(translateDown);
+                break;
+            default:
+                return;
+            }
+            if (moved) {
+                generateRandomNumber();
+            }
+        }
+
         private bool moveLeft()
         {
             bool moved = false;
@@ -146,44 +234,33 @@ namespace Game2048 {
         }
         private void leftButton_Click(object sender, EventArgs e)
         {
-            moveLeft();
-            generateRandomNumber();
+            //moveLeft();
+            if (move(translateLeft)) {
+                generateRandomNumber();
+            }
         }
 
         private void rightButton_Click(object sender, EventArgs e)
         {
-            moveRight();
-            generateRandomNumber();
+            //moveRight();
+            if (move(translateRight)) {
+                generateRandomNumber();
+            }
         }
 
         private void upButton_Click(object sender, EventArgs e)
         {
-            increaseScore(10);
+            if (move(translateUp)) {
+                generateRandomNumber();
+            }
         }
 
         private void downButton_Click(object sender, EventArgs e)
         {
-            increaseScore(-10);
-        }
-
-        private void GameForm_KeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode) {
-            case Keys.Left:
-                moveLeft();
-                break;
-            case Keys.Right:
-                moveRight();
-                break;
-            case Keys.Up:
-                //moveUp();
-                break;
-            case Keys.Down:
-                //moveDown();
-                break;
-            default:
-                break;
+            if (move(translateDown)) {
+                generateRandomNumber();
             }
         }
+
     }
 }
