7dddb94d5bf13a2929b4211a699b5ff2239f6ea0
scgyong <scgyong@gmail.com>
2018-05-14 20:41:25 +0900

    BG, Player image 추가.

--- Runner/Runner/GameForm.Designer.cs
+++ Runner/Runner/GameForm.Designer.cs
@@ -26,10 +26,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 30;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 575);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "GameForm";
             this.Text = "GameForm";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameForm_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer;
     }
 }
--- Runner/Runner/GameForm.cs
+++ Runner/Runner/GameForm.cs
@@ -14,5 +14,62 @@ namespace Runner {
         {
             InitializeComponent();
         }
+
+        Bitmap bgImage;
+        Bitmap bg1, bg2, bg3;
+        int bg1Offset = 0, bg2Offset = 0, bg3Offset = 0;
+
+        Bitmap playerImage;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            bg1Offset -= 8;
+            if (bg1Offset < -800) {
+                bg1Offset += 1400;
+            }
+            bg2Offset -= 14;
+            if (bg2Offset < -261) {
+                bg2Offset += 261;
+            }
+            bg3Offset -= 20;
+            if (bg3Offset < -261) {
+                bg3Offset += 261;
+            }
+            playerFrameIndex = (playerFrameIndex + 1) % 40;
+            //if (++playerFrameIndex >= 4) {
+            //    playerFrameIndex = 0;
+            //}
+            Invalidate();
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(800, 600);
+            bgImage = Runner.Properties.Resources.game_background00;
+            bg1 = Runner.Properties.Resources.game_background01;
+            bg2 = Runner.Properties.Resources.game_background02;
+            bg3 = Runner.Properties.Resources.game_background03;
+            playerImage = Runner.Properties.Resources.game_player;
+        }
+
+        int playerFrameIndex = 0;
+        private void GameForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bgImage, 0, 0, 800, 600);
+            e.Graphics.DrawImage(bg1, bg1Offset, 0, 800, 600);
+
+            for (int x = bg2Offset; x < 800; x += 261) {
+                e.Graphics.DrawImage(bg2, x, 0, 261, 600);
+            }
+            for (int x = bg3Offset; x < 800; x += 261) {
+                e.Graphics.DrawImage(bg3, x, 0, 261, 600);
+            }
+
+            Rectangle dest = new Rectangle(0, 0, 156, 222);
+            e.Graphics.DrawImage(playerImage, dest,
+                (playerFrameIndex / 10) * 156, 0, 156, 222,
+                GraphicsUnit.Pixel);
+        }
+
     }
 }
