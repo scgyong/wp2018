5bac3dcb2429e88417eb1efc9f5d3006870b8438
scgyong <scgyong@gmail.com>
2018-05-31 12:11:46 +0900

    Ball image/class 추가.

---
+++ Bricks/Bricks/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricks {
+    class Ball : GameObject {
+        public Ball() : base(Properties.Resources.spr_ball_0)
+        {
+        }
+    }
+}
